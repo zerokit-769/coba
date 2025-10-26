967dfe6064d9de99bad63f15f1b38deffbfd684d
zerokit-769 <haidliralimutan@gmail.com>
2025-10-26 21:03:37 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ctghjhmuqujl.ap-southeast-1.clawcloudrun.com'
+      url.hostname = 'bhscnvezvhhw.ap-southeast-1.clawcloudrun.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
